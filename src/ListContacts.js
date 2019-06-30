9faab01115f54728ccb670b6936bee5c3e9e1822
tvibhu12 <tvibhu12@gmail.com>
2019-06-30 23:15:14 +0530

    add search function

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'; 
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by';
 
 class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    removeContacts: PropTypes.func.isRequired
+  }
   
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
   
 render(){
-  let contacts=this.props.contacts;
+  let showingContacts
+    if (this.state.query) {
+      const match = new RegExp(escapeRegExp(this.state.query), 'i')
+      showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+
+    } else {
+      showingContacts = this.props.contacts
+    }
+
+    showingContacts.sort(sortBy('name'))
   return(
+    <div className='list-contacts'>
+    {JSON.stringify(this.state)}
+    <div className='list-contacts-top'>
+      <input
+        className='search-contacts'
+        type='text'
+        placeholder='Search contacts'
+        value={this.state.query}
+        onChange={(event) => this.updateQuery(event.target.value)}
+      />
+    </div>
     <ol className='contact-list'>
-      {contacts.map((contact)=>
+      {showingContacts.map((contact)=>
       (
         <li key={contact.id} className='contact-list-item'>
        <div className='contact-avatar' style={{backgroundImage:`url(${contact.avatarURL})`}}/>
@@ -22,6 +56,7 @@ render(){
      )
       )}
     </ol>
+    </div>
   );
 }
 }
