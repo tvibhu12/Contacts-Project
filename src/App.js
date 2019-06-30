7d59e88440c39a3db36ee9e891477a2b0836c666
tvibhu12 <tvibhu12@gmail.com>
2019-06-30 22:55:28 +0530

    add remove contact function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,39 @@ import ListContacts from './ListContacts';
 
 
 class App extends Component {
+  state={
+    contacts : [
+       {
+         "id": "ryan",
+         "name": "Ryan Florence",
+         "email": "ryan@reacttraining.com",
+         "avatarURL": "http://localhost:5001/ryan.jpg"
+       },
+       {
+         "id": "michael",
+         "name": "Michael Jackson",
+         "email": "michael@reacttraining.com",
+         "avatarURL": "http://localhost:5001/michael.jpg"
+       },
+       {
+         "id": "tyler",
+         "name": "Tyler McGinnis",
+         "email": "tyler@reacttraining.com",
+         "avatarURL": "http://localhost:5001/tyler.jpg"
+       }
+     ]
+   }
+   removeContacts=(contact)=>{
+  
+    this.setState((state)=>({
+      contacts:state.contacts.filter((c)=>c.id!==contact.id)
+    }))
+   }
   
   render(){
     return(
       <div>
-      <ListContacts/>
+      <ListContacts contacts={this.state.contacts} removeContacts={this.removeContacts}/>
       </div>
     )
   }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
 
 class ListContacts extends Component {
-  state={
-   contacts : [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
-  }
+  
+  
 render(){
+  let contacts=this.props.contacts;
   return(
     <ol className='contact-list'>
-      {this.state.contacts.map((contact)=>
+      {contacts.map((contact)=>
       (
         <li key={contact.id} className='contact-list-item'>
        <div className='contact-avatar' style={{backgroundImage:`url(${contact.avatarURL})`}}/>
@@ -34,7 +15,7 @@ render(){
          <p>{contact.name}</p>
          <p>{contact.email}</p>
        </div>
-       <button className='contact-remove'>
+       <button className='contact-remove' onClick={()=>this.props.removeContacts(contact)}>
          Remove
        </button>
      </li>
